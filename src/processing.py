94bb95775e6f73a66f3448a70d1f5589e3cd976d
Evgeni <evgeniygek@gmail.com>
2025-04-18 18:02:33 +0700

    add_filter_by_feature_2

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
-def filter_by_state(user_data: list[dict], state: str = 'EXECUTED') -> list:
+def filter_by_state(user_state: list[dict], state: str = "EXECUTED") -> list:
     """Функция сортировки словарей по значению ключа"""
     filter_list = []
-    for key in user_data:
-        if key.get('state') == state:
+    for key in user_state:
+        if key.get("state") == state:
             filter_list.append(key)
     return filter_list
 
+
+def sort_by_date()
--- test/test_processing.py
+++ test/test_processing.py
@@ -1,7 +1,7 @@
-def filter_by_state(user_data: list[dict], state: str = 'EXECUTED') -> list:
+def filter_by_state(user_state: list[dict], state: str = 'EXECUTED') -> list:
     """Функция сортировки словарей по значению ключа"""
     filter_list = []
-    for key in user_data:
+    for key in user_state:
         if key.get('state') == state:
             filter_list.append(key)
     return filter_list
