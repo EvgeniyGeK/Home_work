18deed93235858ee38462b86c57c770130e58ef2
Evgeni <evgeniygek@gmail.com>
2025-04-26 14:01:02 +0700

    created_file_conftest

---
+++ test/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.fixture
+def number():
+    return 1211789458745623
+
+@pytest.fixture
+def account():
+    return 1211789458745623
+
+
+
--- test/test_masks_card_1.py
+++ test/test_masks_card_1.py
@@ -1,3 +1,4 @@
+import pytest
 from src.masks.masks_card import get_mask_card_number
 
 print(get_mask_card_number(1211789458745623))
