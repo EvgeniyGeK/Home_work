58b6e94ba254f84961d8345ad3c50a8722db27bb
Evgeni <evgeniygek@gmail.com>
2025-04-29 22:05:33 +0700

    new_module

---
+++ src/generators.py
@@ -0,0 +1,6 @@
+def filter_by_currency(transactions: list[dict], currency: str) -> dict:
+    """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
+    for i in transactions:
+        if i["operationAmount"]["currency"]["code"] == currency:
+           yield i
+
