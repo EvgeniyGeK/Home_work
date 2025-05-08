53f0642e9c1e55c9d13d7e4d293d013c6efd99eb
Evgeni <evgeniygek@gmail.com>
2025-05-08 14:55:35 +0700

    created_test_generators_black

--- src/generators.py
+++ src/generators.py
@@ -2,9 +2,10 @@ def filter_by_currency(transactions: list[dict], currency: str) -> dict:
     """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
     for i in transactions:
         if i["operationAmount"]["currency"]["code"] == currency:
-           yield i
+            yield i
         # else:
         #    raise "Операций в указанной валюте не найдено"
+
 
 def transaction_descriptions(transactions: list[dict], description: str) -> str:
     """Функция генератор которая принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
