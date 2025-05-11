4a9153eac339905705431cfd08755d509ec0d944
Evgeni <evgeniygek@gmail.com>
2025-05-11 13:42:06 +0700

    created_decorator

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+
+def log(filename=None):
+    """Декоратор автоматически записывает детали выполнения функции."""
+    def decor_func(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                func_result = func(*args, **kwargs)
+                log_func_info = f'{func.__name__} ok'
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as text:
+                        text.write(log_func_info)
+                        text.write('\n')
+                else:
+                    print(log_func_info)
+                return func_result
+            except Exception as e:
+                info_error = f'{func.__name__} Ошибка: {type(e).__name__}. Inputs: {args}, {kwargs}'
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as text:
+                        text.write(info_error)
+                        text.write('\n')
+                else:
+                    print(info_error)
+                raise e
+        return wrapper
+    return decor_func
