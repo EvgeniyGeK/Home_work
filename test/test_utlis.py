14c9037662a4e27134f7c418f7061315d0bc0142
Evgeni <evgeniygek@gmail.com>
2025-05-27 19:53:55 +0700

    Создан модуль теста функции чтения json файла

---
+++ test/test_utlis.py
@@ -0,0 +1,13 @@
+import pytest
+import tempfile
+from unittest.mock import Mock
+
+from src.utlis import read_json_file
+
+
+def test_read_json_file():
+    """Тест функции чтения json файла на ошибки"""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(b'{"key": "value"}')
+        result = read_json_file(temp_file.name)
+        assert result == []
