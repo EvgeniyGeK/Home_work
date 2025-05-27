7576bf5ad8de9acbba40392ec7f0f14f93806c62
Evgeni <evgeniygek@gmail.com>
2025-05-27 21:42:17 +0700

    Созданы тесты для функции чтения json файла2

--- test/test_utlis.py
+++ test/test_utlis.py
@@ -2,7 +2,6 @@ from typing import Any
 
 import pytest
 import tempfile
-from unittest.mock import Mock
 
 from src.utlis import read_json_file
 
@@ -15,17 +14,25 @@ def test_read_json_file():
         assert result == []
 
 
-def test_read_file(test_utlis_1: Any) -> dict:
+def test_read_file(test_utlis_1: Any) -> None:
     """Тест для функции чтения json файла"""
-    assert read_json_file(test_utlis_1) == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
-        'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
-        'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'},
-       {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364',
-        'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}},
-        'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}]
-
-
-
-
-
-
+    assert read_json_file(test_utlis_1) == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560",
+        },
+    ]
