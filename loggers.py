9c0bb0c03b5375b2b843cafe935f954c51518294
Evgeni <evgeniygek@gmail.com>
2025-06-03 23:14:00 +0700

    Логирование функции get_mask_card_number, лог файл создается не там где нужно

--- loggers.py
+++ loggers.py
@@ -8,12 +8,12 @@ def create_log_folder(folder=FOLDER_LOG):
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-def get_logger(name):
-    create_log_folder()
+def get_logger():
+    # create_log_folder()
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(f"log/{__name__}.log", mode='w')
+    handler = logging.FileHandler("__name__.log", mode='a', encoding='UTF-8')
     formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    return logging.getLogger()
+    return logger
--- src/masks/masks_card.py
+++ src/masks/masks_card.py
@@ -1,18 +1,26 @@
+import logging
+from loggers import get_logger
+
+
+logger = get_logger()
+
+logger.info(f"Начало работы функции get_mask_card_number")
 def get_mask_card_number(number_list: str) -> str:
     """Функция маскировки номера банковской карты"""
 
     if len(number_list) != 16:
-
+        logger.error(f'Не верный номер карты')
         return "Не верный номер карты"
-    elif number_list == "":
 
+    elif number_list == "":
+        logger.error(f'Не верный номер карты')
         return "Не верный номер карты"
     else:
-
+        logger.info(f"Функция get_mask_card_number успешно завершила свою работу")
         return f"{str(number_list)[:4]} {str(number_list)[4:6]}** **** {str(number_list)[-4:]}"
 
 
-get_mask_card_number("1234567891023456")
+print(get_mask_card_number("1234567891023456"))
 
 
 def get_mask_account(account_number: str) -> str:
