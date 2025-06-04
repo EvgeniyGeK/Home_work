b7359a707a630c9f6c065818f3daf7f8e21ef009
Evgeni <evgeniygek@gmail.com>
2025-06-04 20:02:07 +0700

    Настройка функции логирования на перезапись

--- loggers.py
+++ loggers.py
@@ -8,7 +8,7 @@ def get_logger(name):
     """Функция для логирования других функций"""
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(PATH / "log" / "__log_info__.log", mode="a", encoding="UTF-8")
+    handler = logging.FileHandler(PATH / "log" / "__log_info__.log", mode="w", encoding="UTF-8")
     formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
