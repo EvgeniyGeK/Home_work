5e50b5db69bba17443b1d5d0935262c51b19df04
Evgeni <evgeniygek@gmail.com>
2025-06-09 18:03:19 +0700

    тесты для функции чтения excel файла

--- test/test_utils_csv_exel.py
+++ test/test_utils_csv_exel.py
@@ -1,6 +1,8 @@
 import tempfile
-from unittest import mock
+import unittest
 from unittest.mock import patch
+
+import pandas as pd
 
 from src.utils_csv_exel import read_exel, read_csv
 
@@ -23,36 +25,27 @@ def test_read_csv():
         assert result == []
 
 
-# @patch("builtins.open")
-@patch("pandas.read_excel")
-def test_read_exel_path(mock_read_excel):
-    """Тест для функции чтения json файла"""
-    mock_file = mock.MagicMock()
-    # mock_open.return_value.__enter__.return_value = mock_file
-    mock_read_excel.return_value = [
-        {
-            "id": "650703.0",
-            "state": "EXECUTED",
-            "date": "2023-09-05T11:30:32Z",
-            "amount": "16210.0",
-            "currency_name": "Sol",
-            "currency_code": "PEN",
-            "from": "Счет 58803664561298323391",
-            "to": "Счет 39745660563456619397",
-            "description": "Перевод организации",
-        }
-    ]
-    assert read_exel(mock_file) == [
-        {
-            "id": 650703.0,
-            "state": "EXECUTED",
-            "date": "2023-09-05T11:30:32Z",
-            "amount": 16210.0,
-            "currency_name": "Sol",
-            "currency_code": "PEN",
-            "from": "Счет 58803664561298323391",
-            "to": "Счет 39745660563456619397",
-            "description": "Перевод организации",
-        }
-    ]
-    mock_read_excel.assert_called_with(mock_file)
+class TestReadExel(unittest.TestCase):
+
+    @patch('pandas.read_excel')
+    def test_read_exel_success(self, mock_read_excel):
+        """Тест на чтение excel файлов функцией read_exel"""
+        df_mock = pd.DataFrame({'Name': ['Alice'], 'Age': [25]})
+        mock_read_excel.return_value = df_mock
+        path_to_file = 'test.xlsx'
+        result = read_exel(path_to_file)
+        expected_result = [{'Name': 'Alice', 'Age': 25}]
+        self.assertEqual(result, expected_result)
+
+    @patch('pandas.read_excel')
+    def test_read_exel_failure(self, mock_read_excel):
+        """Тест на ошибку функцией read_exel"""
+        mock_read_excel.side_effect = FileNotFoundError("Файл не найден")
+        path_to_file = 'nonexistent_file.xlsx'
+        result = read_exel(path_to_file)
+        self.assertEqual(result, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
