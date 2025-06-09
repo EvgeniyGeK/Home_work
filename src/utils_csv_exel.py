0ea7e9b095a22cfe735d244533bc1da8ec4dbd68
Evgeni <evgeniygek@gmail.com>
2025-06-09 18:15:56 +0700

    Оформление файлов

--- src/utils_csv_exel.py
+++ src/utils_csv_exel.py
@@ -6,10 +6,10 @@ import pandas as pd
 def read_exel(path_file: str) -> list:
     """Функция читает exel файлов и преобразует данные в список словарей"""
     try:
-        exel_data = pd.read_excel(path_file).to_dict(orient='records')
+        exel_data = pd.read_excel(path_file).to_dict(orient="records")
         return exel_data
     except Exception as e:
-        Exception(f'Произошла ошибка: {e}')
+        Exception(f"Произошла ошибка: {e}")
         return []
 
 
@@ -18,12 +18,11 @@ def read_csv(path_to_file: str) -> list:
 
     try:
         csv_dict = []
-        with open(path_to_file, encoding='utf-8') as file:
-            csv_data = csv.DictReader(file, delimiter=';')
+        with open(path_to_file, encoding="utf-8") as file:
+            csv_data = csv.DictReader(file, delimiter=";")
             for row in csv_data:
                 csv_dict.append(row)
             return csv_dict
     except Exception as e:
-        Exception(f'Произошла ошибка: {e}')
+        Exception(f"Произошла ошибка: {e}")
         return []
-
--- test/test_utils_csv_exel.py
+++ test/test_utils_csv_exel.py
@@ -28,49 +28,49 @@ def test_read_csv():
 
 class TestReadExel(unittest.TestCase):
 
-    @patch('pandas.read_excel')
+    @patch("pandas.read_excel")
     def test_read_exel_success(self, mock_read_excel):
         """Тест на чтение excel файлов функцией read_exel"""
-        df_mock = pd.DataFrame({'Name': ['Alice'], 'Age': [25]})
+        df_mock = pd.DataFrame({"Name": ["Alice"], "Age": [25]})
         mock_read_excel.return_value = df_mock
-        path_to_file = 'test.xlsx'
+        path_to_file = "test.xlsx"
         result = read_exel(path_to_file)
-        expected_result = [{'Name': 'Alice', 'Age': 25}]
+        expected_result = [{"Name": "Alice", "Age": 25}]
         self.assertEqual(result, expected_result)
 
-    @patch('pandas.read_excel')
+    @patch("pandas.read_excel")
     def test_read_exel_failure(self, mock_read_excel):
         """Тест на ошибку функцией read_exel"""
         mock_read_excel.side_effect = FileNotFoundError("Файл не найден")
-        path_to_file = 'nonexistent_file.xlsx'
+        path_to_file = "nonexistent_file.xlsx"
         result = read_exel(path_to_file)
         self.assertEqual(result, [])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
 
 class TestReadCsv(unittest.TestCase):
 
-    @patch('builtins.open')
+    @patch("builtins.open")
     def test_read_csv_success(self, mock_open):
         """Тест на чтение csv файлов функцией read_csv"""
-        fake_csv_content = u"""Name;Age\nJohn;30\nJane;25"""
+        fake_csv_content = """Name;Age\nJohn;30\nJane;25"""
         mock_open.return_value.__enter__.return_value = io.StringIO(fake_csv_content)
-        path_to_file = 'fake.csv'
+        path_to_file = "fake.csv"
         result = read_csv(path_to_file)
         expected_result = [{"Name": "John", "Age": "30"}, {"Name": "Jane", "Age": "25"}]
         self.assertEqual(result, expected_result)
 
-    @patch('builtins.open')
+    @patch("builtins.open")
     def test_read_csv_error(self, mock_open):
         """Тест на ошибку функцией read_csv"""
         mock_open.side_effect = IOError("File not found")
-        path_to_file = 'invalid_path.csv'
+        path_to_file = "invalid_path.csv"
         result = read_csv(path_to_file)
         self.assertEqual(result, [])
 
+
 if __name__ == "__main__":
     unittest.main()
-
