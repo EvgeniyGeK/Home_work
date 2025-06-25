81e5f3245837596d2e6bafe6fbb33baa5d4cc428
Evgeni <evgeniygek@gmail.com>
2025-06-25 23:11:51 +0700

    Фильтрация работает

--- src/utils_csv_exel.py
+++ src/utils_csv_exel.py
@@ -1,5 +1,5 @@
 import csv
-
+import openpyxl
 import pandas as pd
 
 
@@ -9,8 +9,8 @@ def read_exel(path_file: str) -> list:
         exel_data = pd.read_excel(path_file).to_dict(orient="records")
         return exel_data
     except Exception as e:
-        Exception(f"Произошла ошибка: {e}")
-        return []
+        raise Exception(f"Произошла ошибка: {e}")
+
 
 
 def read_csv(path_to_file: str) -> list:
